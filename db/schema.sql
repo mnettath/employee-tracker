1c670c77998351db63d547ca73264dc984d6d957
Maya N <nettathm@gmail.com>
2023-09-27 15:14:42 -0600

    added sql files in the db folder, created server.js, imported node modules with gitignore

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- tables go here -- 
+
+-- create department table --
+
+-- create role table -- 
+
+-- create employee table -- 
