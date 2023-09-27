3036a753e460d926795f36fde6731d0a4e99f371
Maya N <nettathm@gmail.com>
2023-09-27 15:30:32 -0600

    updated schema.sql so that syntax was correct for workbench

--- db/schema.sql
+++ db/schema.sql
@@ -7,31 +7,37 @@ USE employees_db;
 
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 
-CREATE TABLE [department] (
-    [id] INT  NOT NULL ,
-    [name] VARCHAR(30)  NOT NULL ,
-    CONSTRAINT [PK_department] PRIMARY KEY CLUSTERED (
-        [id] ASC
+USE employees_db;
+
+CREATE TABLE department (
+    id INT  NOT NULL ,
+    name VARCHAR(30) NOT NULL ,
+    CONSTRAINT PK_department PRIMARY KEY CLUSTERED (
+        id ASC
+    )
+);
+
+CREATE TABLE role (
+    id INT  NOT NULL ,
+    title VARCHAR(30)  NOT NULL ,
+    salary DECIMAL  NOT NULL ,
+    department_id INT  NOT NULL ,
+    CONSTRAINT PK_role PRIMARY KEY CLUSTERED (
+        id ASC
+    )
+);
+
+CREATE TABLE employee (
+    id INT  NOT NULL ,
+    first_name VARCHAR(30)  NOT NULL ,
+    last_name VARCHAR(30)  NOT NULL ,
+    role_id INT  NOT NULL ,
+    manager_id INT  NULL ,
+    CONSTRAINT PK_employee PRIMARY KEY CLUSTERED (
+        id ASC
     )
 )
 
-CREATE TABLE [role] (
-    [id] INT  NOT NULL ,
-    [title] VARCHAR(30)  NOT NULL ,
-    [salary] DECIMAL  NOT NULL ,
-    [department_id] INT  NOT NULL ,
-    CONSTRAINT [PK_role] PRIMARY KEY CLUSTERED (
-        [id] ASC
-    )
-)
-
-CREATE TABLE [employee] (
-    [id:] INT  NOT NULL ,
-    [first_name] VARCHAR(30)  NOT NULL ,
-    [last_name] VARCHAR(30)  NOT NULL ,
-    [role_id] INT  NOT NULL ,
-    [manager_id] INT  NULL ,
-    CONSTRAINT [PK_employee] PRIMARY KEY CLUSTERED (
-        [id:] ASC
-    )
-)
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
