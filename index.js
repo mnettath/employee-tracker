365482e19743b2e111463bc60ae898e6a2d09f29
Maya N <nettathm@gmail.com>
2023-09-27 15:54:26 -0600

    changed server.js to index.js, added inquirer package to display questions, got rid of port

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db`)
+);
+
+inquirer.prompt([
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View all Employees",
+      "Add Employee",
+      "Update Employee Role",
+      "View All Roles",
+      "Add Role",
+      "View All Departments",
+      "Add Department",
+      "Quit",
+    ],
+    name: "selection",
+  },
+]);
