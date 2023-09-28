b4fc4efe2cae1947df577c7ffd3541364a3dc14e
Maya N <nettathm@gmail.com>
2023-09-27 20:25:28 -0600

    added a view all employees function and a switch

--- index.js
+++ index.js
@@ -11,24 +11,39 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db`)
 );
 
-inquirer.prompt([
-  {
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      "View all Employees",
-      "Add Employee",
-      "Update Employee Role",
-      "View All Roles",
-      "Add Role",
-      "View All Departments",
-      "Add Department",
-      "Quit",
-    ],
-    name: "selection",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+      name: "selection",
+    },
+  ])
+  .then((data) => {
+    switch (data.selection) {
+      case "View all Employees":
+        viewAllEmployees();
+        break;
+    }
+  });
 
-db.query("SELECT * FROM movies", (err, results) => {
-  console.log(err);
-});
+// need to add code: when user selects View all Employees, the table for employee must populate
+function viewAllEmployees() {
+  db.query("SELECT * FROM employee", (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(results);
+    }
+  });
+}
